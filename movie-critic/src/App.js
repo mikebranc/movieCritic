3d7317066e777b369950de05fa975fa928e865b3
jf2024 <>
2021-04-02 19:16:27 -0700

    Imported Navbar to make it show up

--- movie-critic/src/App.js
+++ movie-critic/src/App.js
@@ -1,29 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import Searchbar from './Components/Searchbar'
-import Search from './Pages/Search'
-import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import Searchbar from "./Components/Searchbar";
+import Search from "./Pages/Search";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
 
 function App() {
-  
   return (
     <Router>
       <>
-      {/* A <Switch> looks through its children <Route>s and
+        {/* A <Switch> looks through its children <Route>s and
           renders the first one that matches the current URL. */}
-      <Switch>
-        <Route path="/about">
-          <Searchbar />
-        </Route>
-        <Route path="/search">
-          <Search />
-        </Route>
-        <Route path="/">
-          <Searchbar />
-        </Route>
-      </Switch>
-    </>
-  </Router>
+        <Switch>
+          <Route path="/about">
+            <Searchbar />
+          </Route>
+          <Route path="/search">
+            <Search />
+            <Navbar />
+          </Route>
+          <Route path="/">
+            <Searchbar />
+          </Route>
+        </Switch>
+      </>
+    </Router>
   );
 }
 
