5ca372d96b4ef7af795e9613bc99575966415047
Maya Dahlke <mayadahlke@yahoo.com>
2021-04-14 13:57:55 -0700

    change home button for post

--- movie-critic/src/Pages/Home.jsx
+++ movie-critic/src/Pages/Home.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React from 'react';
+import '../Style.css';
+import MakeAPost from '../Components/MakeAPost';
 
-import '../Style.css'
-
-
-export default function Home(){
-    return(
-        <div style={{ height:'100vh', backgroundPosition:'center',  backgroundSize: 'cover'}}>
-            <div style={{backgroundColor: '#2A363B', height:'100%'}}>
-                <div style={{opacity: '1', display: 'flex', alignItems: 'center',  paddingTop: 100, flexDirection: 'column', width:'100%'}}>
-                    <div style={{display:'flex', justifyContent:'center', alignItems: 'center', backgroundColor:'#99B898', width: '60%', height:'70px', borderRadius:10, fontSize:'30px', fontWeight:'bold', cursor:'pointer' }}>Make a Post</div>
-                </div>
-            </div>
+export default function Home() {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+      }}
+    >
+      <div style={{ backgroundColor: '#2A363B', height: '100%' }}>
+        <div
+          style={{
+            opacity: '1',
+            display: 'flex',
+            alignItems: 'center',
+            paddingTop: 100,
+            flexDirection: 'column',
+            width: '100%',
+          }}
+        >
+          <MakeAPost />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
