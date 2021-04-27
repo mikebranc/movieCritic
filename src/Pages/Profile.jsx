e7563c7c74df0fe97ba3c778620d4f0db66aee46
jf2024 <>
2021-04-27 10:56:04 -0700

    added bio

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,39 +1,47 @@
 // change watchlistcell everywhere on here
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 import { UserContext } from "../UserProvider";
-import {auth} from "../firebase/firebase.util";
-import MustWatch from '../Components/MustWatch';
-import '../Style.css';
+import { auth } from "../firebase/firebase.util";
+import MustWatch from "../Components/MustWatch";
+import Bio from "../Components/Bio";
+import "../Style.css";
 
 export default function Profile() {
   //const [results, setResults] = useState([])
-  const user = useContext(UserContext)
+  const user = useContext(UserContext);
   //const {email, username} = user;
-  console.log(user)
+  console.log(user);
   return (
     <div>
       <div
         style={{
-          height: '100vh',
-          backgroundPosition: 'center',
-          backgroundSize: 'cover',
-          backgroundColor: '#2A363B',
-          position: 'relative',
+          height: "100vh",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundColor: "#2A363B",
+          position: "relative",
         }}
       >
         <div
           style={{
-            position: 'relative',
-            top: '100px',
-            padding: '0 20px 0 20px',
+            position: "relative",
+            top: "100px",
+            padding: "0 20px 0 20px",
           }}
         >
           <MustWatch />
-
+          <Bio />
         </div>
       </div>
-      <button className = "w-full py-3 bg-red-600 mt-4 text-white" onClick = {() => {auth.signOut(); window.location.href = '/signin'}}>Sign out</button>
+      <button
+        className="w-full py-3 bg-red-600 mt-4 text-white"
+        onClick={() => {
+          auth.signOut();
+          window.location.href = "/signin";
+        }}
+      >
+        Sign out
+      </button>
     </div>
-
   );
 }
