e78243fac9839c31a94187f36cf61843101938b5
jf2024 <>
2021-04-27 18:18:44 -0700

    updated profile page

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -4,6 +4,10 @@ import { UserContext } from "../UserProvider";
 import { auth } from "../firebase/firebase.util";
 import MustWatch from "../Components/MustWatch";
 import Bio from "../Components/Bio";
+import FeaturedReview from "../Components/FeaturedReview";
+import Reviews from "../Components/Reviews";
+import FavMovie from "../Components/FavMovie";
+import UserProfile from "../Components/UserProfile";
 import "../Style.css";
 
 export default function Profile() {
@@ -15,7 +19,7 @@ export default function Profile() {
     <div>
       <div
         style={{
-          height: "100vh",
+          height: "150vh",
           backgroundPosition: "center",
           backgroundSize: "cover",
           backgroundColor: "#2A363B",
@@ -25,12 +29,61 @@ export default function Profile() {
         <div
           style={{
             position: "relative",
-            top: "100px",
+            top: "300px",
             padding: "0 20px 0 20px",
           }}
         >
           <MustWatch />
+        </div>
+        <div
+          style={{
+            position: "relative",
+            top: "-100px",
+            marginLeft: "350px",
+            padding: "0 20px 0 20px",
+          }}
+        >
+          <FeaturedReview />
+        </div>
+        <div
+          style={{
+            position: "relative",
+            top: "-525px",
+            marginLeft: "825px",
+            padding: "0 20px 0 20px",
+          }}
+        >
           <Bio />
+        </div>
+        <div
+          style={{
+            position: "relative",
+            top: "-280px",
+            marginLeft: "350px",
+            padding: "0 20px 0 20px",
+          }}
+        >
+          <Reviews />
+        </div>
+        <div
+          style={{
+            position: "relative",
+            top: "-850px",
+            marginLeft: "750px",
+            padding: "0 20px 0 20px",
+          }}
+        >
+          <FavMovie />
+        </div>
+        <div
+          style={{
+            position: "relative",
+            top: "-1275px",
+            marginLeft: "0px",
+            padding: "0 20px 0 20px",
+          }}
+        >
+          <UserProfile />
         </div>
       </div>
       <button
